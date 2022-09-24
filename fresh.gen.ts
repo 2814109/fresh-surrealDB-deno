cdb8292079538f3e0f71152d94c535fff546468a
kirke <kirke@kirkenoMacBook-Air.local>
2022-09-25 06:45:21 +0900

    create sample code

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,19 +3,25 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $0 from "./routes/api/delete.ts";
+import * as $1 from "./routes/api/get.ts";
+import * as $2 from "./routes/api/isdone.ts";
+import * as $3 from "./routes/api/post.ts";
+import * as $4 from "./routes/index.tsx";
+import * as $$0 from "./islands/Box.tsx";
+import * as $$1 from "./islands/Item.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/api/delete.ts": $0,
+    "./routes/api/get.ts": $1,
+    "./routes/api/isdone.ts": $2,
+    "./routes/api/post.ts": $3,
+    "./routes/index.tsx": $4,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
+    "./islands/Box.tsx": $$0,
+    "./islands/Item.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,
